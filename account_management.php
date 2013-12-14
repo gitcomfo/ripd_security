69873d0fc595e9a8c15a84ebb87a1435d1f407de
smrity <smrity48@gmail.com>
2013-12-14 13:25:56 +0600

    current_loan.php, previous_loan_statement.php , account_management.php ..................

--- account_management.php
+++ account_management.php
@@ -9,129 +9,168 @@ include_once 'includes/MiscFunctions.php';
 $session_user_id = $_SESSION['userIDUser'];
 $sql_select_cfs_user_all->execute(array($session_user_id));
 $arr_cfs_user = $sql_select_cfs_user_all->fetchAll();
-foreach ($arr_cfs_user as $acu) 
-        {
-        $aab_account_name = $acu['account_name'];
-        $aab_account_number = $acu['account_number'];
-        $aab_open_date = english2bangla($acu['account_open_date']);
-        $aab_mobile = english2bangla($acu['mobile']);
-        $aab_email = $acu['email'];
-       $aab_user_type = $acu['user_type'];
-        $cfs_user_id = $acu['idUser'];
-        }
-if($aab_user_type == 'customer')
-{
+foreach ($arr_cfs_user as $acu) {
+    $aab_account_name = $acu['account_name'];
+    $aab_account_number = $acu['account_number'];
+    $aab_open_date = english2bangla($acu['account_open_date']);
+    $aab_mobile = english2bangla($acu['mobile']);
+    $aab_email = $acu['email'];
+    $aab_user_type = $acu['user_type'];
+    $cfs_user_id = $acu['idUser'];
+}
+if ($aab_user_type == 'customer') {
     $sql_select_cust_basic->execute(array($cfs_user_id));
     $arr_cust_basic = $sql_select_cust_basic->fetchAll();
-    foreach ($arr_cust_basic as $aab) 
-            {
-            $aab_designation_name = $aab['designation_name'];
-            $aab_designation_star = english2bangla($aab['designation_star']);
-            $aab_picture = $aab['scanDoc_picture'];
-            $aab_referrer_id = $aab['referer_id'];
-            }
+    foreach ($arr_cust_basic as $aab) {
+        $aab_designation_name = $aab['designation_name'];
+        $aab_designation_star = english2bangla($aab['designation_star']);
+        $aab_picture = $aab['scanDoc_picture'];
+        $aab_referrer_id = $aab['referer_id'];
+    }
     $sql_select_cfs_user_all->execute(array($aab_referrer_id));
     $arr_referrer = $sql_select_cfs_user_all->fetchAll();
-    foreach ($arr_referrer as $ar) $ar_referrer = $ar['account_name'];
-}
-elseif($aab_user_type == 'owner')
-{
+    foreach ($arr_referrer as $ar)
+        $ar_referrer = $ar['account_name'];
+} elseif ($aab_user_type == 'owner') {
     $sql_select_propritor_basic->execute(array($cfs_user_id));
     $arr_proprietor_basic = $sql_select_propritor_basic->fetchAll();
-    foreach ($arr_proprietor_basic as $aab) 
-            {
-                $aab_picture = $aab['prop_scanDoc_picture'];
-            }
-}
-else
-{
+    foreach ($arr_proprietor_basic as $aab) {
+        $aab_picture = $aab['prop_scanDoc_picture'];
+    }
+} else {
     $sql_select_employee_basic->execute(array($cfs_user_id));
     $arr_employee_basic = $sql_select_employee_basic->fetchAll();
-    foreach ($arr_employee_basic as $aab) 
-            {
-               $aab_picture = $aab['emplo_scanDoc_picture'];
-            }
+    foreach ($arr_employee_basic as $aab) {
+        $aab_picture = $aab['emplo_scanDoc_picture'];
+    }
 }
-if(!file_exists($aab_picture)) $aab_picture = "pic/default_profile.jpg";
+if (!file_exists($aab_picture))
+    $aab_picture = "pic/default_profile.jpg";
 ?>
 
 <title>প্রোফাইল ম্যানেজমেন্ট</title>
 <style type="text/css">@import "css/domtab.css";</style>
 
 <div class="columnSubmodule">
-        <table class="formstyle">    
-                <tr>
-                    <th colspan="2" style="font-size: 15px;">একাউন্ট নম্বরঃ <?php echo $aab_account_number;?></th>
-                </tr>
-                <tr>
-                    <?php
-                    if($aab_user_type == "customer"){
-                    ?>
-                    <td>
-                        <table>
-                            <tr>
-                                <td style='font-size: 20px; text-align:center;' colspan="2"><b>আপনার একাউন্টে স্বাগতম</b></td>
-                            </tr>                            
-                            <tr>
-                                <td><b>রেফারার নামঃ </b></td>
-                                <td><?php echo $ar_referrer;?></td>
-                            </tr>
-                            <tr>
-                                <td><b>একাউন্ট খোলার তারিখঃ </b></td>
-                                <td><?php echo $aab_open_date;?></td>
-                            </tr>
-                            <tr>
-                                <td><b>ডেজিগনেশনঃ </b></td>
-                                <td><?php echo "$aab_designation_name ($aab_designation_star স্টার)";?></td>
-                            </tr>
-                            <tr>
-                                <td><b>একাউন্ট টাইপঃ </b></td>
-                                <td>সেটেল একাউন্ট</td>
-                            </tr>
-                            <tr>
-                                <td><b>এমাউন্টঃ </b></td>
-                                <td>৫৪৮৭ টাকা</td>
-                            </tr>
-                        </table>
-                    </td>
-                    <?php
-                    }
-                    else echo "<td style='font-size: 20px; text-align:center;'><b>আপনার একাউন্টে স্বাগতম</b></td>";
-                    ?>
-                    <td style="width: 35%; text-align: center;">
-                        <table >
-                            <tr>
-                                <td style="font-size: 16px; text-align: center;"><b><?php echo $aab_account_name;?></b></td>
-                            </tr>
-                            <tr>
-                                <td style="text-align: center;"><img src="<?php echo $aab_picture;?>" width='120px' height='120px'/></td>
-                            </tr>
-                            <tr>
-                                <td style="text-align: center;"><?php echo $aab_mobile;?></td>
-                            </tr>
-                            <tr>
-                                <td style="text-align: center;"><?php echo $aab_email;?></td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-                <?php
-                if($aab_user_type == "customer"){
+    <table class="formstyle">    
+        <tr>
+            <th colspan="2" style="font-size: 15px;">একাউন্ট নম্বরঃ <?php echo $aab_account_number; ?></th>
+        </tr>
+        <tr>
+            <?php
+            if ($aab_user_type == "customer") {
                 ?>
-                <tr>
-                    <td colspan="2"><hr style="height: 4px;"></hr></td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <table>
-                            here some PV report will be kept
-                        </table>
-                    </td>
-                </tr>
+                <td>
+                    <table>
+                        <tr>
+                            <td style='font-size: 20px; text-align:center;' colspan="2"><b>আপনার একাউন্টে স্বাগতম</b></td>
+                        </tr>                            
+                        <tr>
+                            <td><b>রেফারার নামঃ </b></td>
+                            <td><?php echo $ar_referrer; ?></td>
+                        </tr>
+                        <tr>
+                            <td><b>একাউন্ট খোলার তারিখঃ </b></td>
+                            <td><?php echo $aab_open_date; ?></td>
+                        </tr>
+                        <tr>
+                            <td><b>ডেজিগনেশনঃ </b></td>
+                            <td><?php echo "$aab_designation_name ($aab_designation_star স্টার)"; ?></td>
+                        </tr>
+                        <tr>
+                            <td><b>একাউন্ট টাইপঃ </b></td>
+                            <td>সেটেল একাউন্ট</td>
+                        </tr>
+                        <tr>
+                            <td><b>এমাউন্টঃ </b></td>
+                            <td>৫৪৮৭ টাকা</td>
+                        </tr>
+                    </table>
+                </td>
+
                 <?php
-                    }
+            }
+            else
+                
                 ?>
-        </table>
+            <td>
+                <table>
+                    <tr>
+                        <td style='font-size: 20px; text-align:center;' colspan="2"><b>আপনার একাউন্টে স্বাগতম</b></td>
+                    </tr>                            
+                    <tr>
+                        <td><b>কর্মরত অফিস নামঃ </b></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><b>অফিস নাম্বারঃ </b></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><b>অফিস ঠিকানাঃ </b></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><b> যোগদানের তারিখঃ </b></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><b>বর্তমান গ্রেডঃ </b></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><b>দায়িত্বঃ </b></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </td>
+
+            <td style="width: 35%; text-align: center;">
+                <table >
+                    <tr>
+                        <td style="font-size: 16px; text-align: center;"><b><?php echo $aab_account_name; ?></b></td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: center;"><img src="<?php echo $aab_picture; ?>" width='120px' height='120px'/></td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: center;"><?php echo $aab_mobile; ?></td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: center;"><?php echo $aab_email; ?></td>
+                    </tr>
+                </table>
+            </td>
+        <tr>
+            <td colspan="2" ><hr /></td>
+        </tr>
+        <tr>
+            <td style="padding-left: 20px;"><b>চাকরিতে সর্বপ্রথম যোগদানঃ </b></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td style="padding-left: 20px;"><b>ইন গ্রেডঃ </b></td>
+            <td></td>
+        </tr>
+        </tr>
+        <?php
+        if ($aab_user_type == "customer") {
+            ?>
+            <tr>
+                <td colspan="2"><hr style="height: 4px;"></hr></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <table>
+                        here some PV report will be kept
+                    </table>
+                </td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
 </div>
 
 <?php
---
+++ previous_loan_statement.php
@@ -0,0 +1,59 @@
+<?php
+//include_once 'includes/session.inc';
+include_once 'includes/header.php';
+include_once 'includes/MiscFunctions.php';
+
+?>
+<title>প্রিভিইয়াস স্টেটমেন্ট</title>
+<style type="text/css"> @import "css/bush.css";</style>
+<link rel="stylesheet" href="css/tinybox.css" type="text/css" media="screen" charset="utf-8"/>
+<script src="javascripts/tinybox.js" type="text/javascript"></script>
+
+<div class="main_text_box">
+    <div style="padding-left: 110px;"><a href="#"><b>ফিরে যান</b></a></div>
+    <div>
+        <table  class="formstyle" style="font-family: SolaimanLipi !important;width: 80%;">          
+            <tr><th colspan="2" style="text-align: center;">প্রিভিয়াস লোন স্টেটমেন্ট</th></tr>
+            <tr>
+                <td colspan="2" ><hr /></td>
+            </tr>
+            <tr >
+                <td style="text-align: right; width: 50%"><b>মোট লোন সংখ্যাঃ</b></td>
+                <td></td>
+            </tr>
+            <tr >
+                <td style="text-align: right; width: 50%"><b>টোটাল লোন এমাউন্টঃ</b></td>
+                <td></td>
+            </tr>
+          
+
+            <tr>
+                <td colspan="2">
+                    <table border="1" align="center" width= 99%" cellpadding="5px" cellspacing="0px">    
+                            <tr  id="table_row_odd">
+                                <td style="border: black 1px solid; text-align: center">ক্রম</td>
+                                <td style="border: black 1px solid; text-align: center">শুরুর তারিখ</td>
+                                <td style="border: black 1px solid; text-align: center" >শেষের এমাউন্ট</td>
+                                <td style="border: black 1px solid; text-align: center">কিস্তি</td>
+                                <td style="border: black 1px solid; text-align: center">টোটাল এমাউন্ট</td>
+                            </tr>
+                        
+                            <tr>
+                                <td style="border: black 1px solid; text-align: center">1</td>
+                                <td style="border: black 1px solid; text-align: center">1</td>
+                                <td style="border: black 1px solid; text-align: center">1</td>
+                                <td style="border: black 1px solid; text-align: center">1</td>
+                                <td style="border: black 1px solid; text-align: center">1</td>
+                            </tr> 
+                       
+                        </table>
+                </td>
+            </tr>
+        </table>
+
+    </div>           
+</div>
+<?php
+
+include_once 'includes/footer.php';
+?>
