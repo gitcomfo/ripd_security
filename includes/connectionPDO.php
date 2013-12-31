3a4f0e4fe8e7e02e173b546cb4646f8ed5d2a6d1
jesy <jesy@jesy-PC>
2013-12-31 11:08:58 +0600

    pos system commit by jesy

--- includes/connectionPDO.php
+++ includes/connectionPDO.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 
-//$dbhost = "192.168.1.100";
-//$dbname = "ripd_db_comfosys";
-//$dbuser = "cfs_jessy";
-//$dbpass = "jesy4321";
+$dbhost = "192.168.1.100";
+$dbname = "ripd_db_comfosys";
+$dbuser = "cfs_jessy";
+$dbpass = "jesy4321";
 
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'ripd_db_comfosys';
+//$dbhost = 'localhost';
+//$dbuser = 'root';
+//$dbpass = '';
+//$dbname = 'ripd_db_comfosys';
 
 // database connection
 $conn = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
