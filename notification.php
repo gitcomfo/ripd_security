3e30d55bd38aec9798649531f4f26ce6f37de880
jesy <jesy@jesy-PC>
2014-01-21 12:22:15 +0600

    commit by jesy

--- notification.php
+++ notification.php
@@ -24,36 +24,44 @@ $logedinOfficeType = $_SESSION['loggedInOfficeType'];
                     $sel_official_notification = $conn->prepare("SELECT * FROM ons_relation, notification WHERE catagory=? AND add_ons_id=?
                        AND idons_relation=nfc_receiverid AND nfc_status !='complete' AND nfc_catagory =?");
                     $sel_official_notification ->execute(array($logedinOfficeType,$logedinOfficeId,$catagory));
-                    $notificationrow = $sel_official_notification->fetchAll();                    
-                    foreach ($notificationrow as $value)
-                         {
-                            $db_nfc_id = $value['idnotification'];
-                            $db_msg = $value['nfc_message'];
-                            $db_status = $value['nfc_status'];
-                            $db_url = $value['nfc_actionurl'];
-                            $db_type = $value['nfc_type'];
-                            //$status = $arr_notification_status[$db_status];
-                            if($db_status == 'unread')
-                            {
-                                echo "<tr style='background-color:#ffcc99'>";
-                            }
-                            else {
-                             echo "<tr>";   
-                            }
-                            echo "<td>".english2bangla($db_slNo)."</td>";
-                            echo "<td>$db_msg</td>";
-//                            echo "<td>$status</td>";
-                            if($db_type == 'action')
-                            {
-                                echo "<td><a href='notification_gateway.php?url=$db_url&nfcid=$db_nfc_id' ><b>দেখুন</b></a></td>";
-                            }
-                            else
-                            {
-                                echo "<td></td>";
-                            }
-                            echo "</tr>";
-                            $db_slNo++;
-                         }          
+                    $notificationrow = $sel_official_notification->fetchAll();
+                    $countrow = count($notificationrow);
+                    if($countrow == 0)
+                    {
+                        echo "<tr><td colspan = '3' style='color:red;text-align:center;'> এই মুহূর্তে কোন অফিসিয়াল নোটিফিকেশন নেই</td></tr>";
+                    }
+                    else 
+                    {
+                        foreach ($notificationrow as $value)
+                             {
+                                $db_nfc_id = $value['idnotification'];
+                                $db_msg = $value['nfc_message'];
+                                $db_status = $value['nfc_status'];
+                                $db_url = $value['nfc_actionurl'];
+                                $db_type = $value['nfc_type'];
+                                //$status = $arr_notification_status[$db_status];
+                                if($db_status == 'unread')
+                                {
+                                    echo "<tr style='background-color:#ffcc99'>";
+                                }
+                                else {
+                                 echo "<tr>";   
+                                }
+                                echo "<td>".english2bangla($db_slNo)."</td>";
+                                echo "<td>$db_msg</td>";
+    //                            echo "<td>$status</td>";
+                                if($db_type == 'action')
+                                {
+                                    echo "<td><a href='notification_gateway.php?url=$db_url&nfcid=$db_nfc_id' ><b>দেখুন</b></a></td>";
+                                }
+                                else
+                                {
+                                    echo "<td></td>";
+                                }
+                                echo "</tr>";
+                                $db_slNo++;
+                             }
+                    }
 ?>
                     </tbody>
             </table>                        
