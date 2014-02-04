8624a616f3afaaf8df8c316a9904a45e968203cf
jesy <jesy@jesy-PC>
2014-02-04 16:58:18 +0600

    commit by jesy

--- personal_purchase_statement.php
+++ personal_purchase_statement.php
@@ -1,174 +1,8 @@
 <?php
 //include 'includes/session.inc';
 include_once 'includes/header.php';
-$msg = "";
-
-function get_catagory() {
-    echo "<option value=0> -সিলেক্ট করুন- </option>";
-    $catagoryRslt = mysql_query("SELECT DISTINCT pro_catagory, pro_cat_code FROM product_catagory ORDER BY pro_catagory;");
-    while ($catrow = mysql_fetch_assoc($catagoryRslt)) {
-        echo "<option value=" . $catrow['pro_cat_code'] . ">" . $catrow['pro_catagory'] . "</option>";
-    }
-}
 ?>
 <style type="text/css">@import "css/bush.css";</style>
-<link rel="stylesheet" href="css/tinybox.css" type="text/css">
-<script src="javascripts/tinybox.js" type="text/javascript"></script>
-<script type="text/javascript">
-    function detailsWithPrice()
-    { TINY.box.show({url:'includes/ripd_product_details_price.php',width:550,height:530,opacity:30,topsplit:3,animate:true,close:true,maskid:'bluemask',maskopacity:50,boxid:'success'}); }
-</script>
-<!--===========================================================================================================================-->
-<script>
-    function showTypes(catagory) // for types dropdown list
-    {
-        var xmlhttp;
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                document.getElementById('showtype').innerHTML=xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET","pos/includes/searchProcessForAll.php?id=t&catagory="+catagory,true);
-        xmlhttp.send();	
-    }
-    function showBrands(type) // for brand dropdown list
-    {
-        var xmlhttp;
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                document.getElementById('brand').innerHTML=xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET","pos/includes/searchProcessForAll.php?id=b&type="+type,true);
-        xmlhttp.send();	
-    }
-    function showClass(brand,protype) // for product name dropdown list
-    {
-        var xmlhttp;
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                document.getElementById('classi').innerHTML=xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET","pos/includes/searchProcessForAll.php?id=c&brand="+brand+"&type="+protype,true);
-        xmlhttp.send();	
-    }
-    function showProduct(productChartId,idbrand,cataID) // show product details from selecting product from dropdown
-    {
-        var xmlhttp;
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                document.getElementById('resultTable').innerHTML=xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET","pos/includes/searchProcessForAll.php?id=all&chartID="+productChartId+"&idbrand="+idbrand+"&cataID="+cataID,true);
-        xmlhttp.send();
-    }
-    function showCatProducts(code) // show products from selecting catagory
-    {
-        var xmlhttp;
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                document.getElementById('resultTable').innerHTML=xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET","pos/includes/searchProcessForAll.php?id=catagory&proCatCode="+code,true);
-        xmlhttp.send();
-    }
-    function showTypeProducts(proCatID) // show products from selecting types
-    {
-        var xmlhttp;
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                document.getElementById('resultTable').innerHTML=xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET","pos/includes/searchProcessForAll.php?id=type&proCatID="+proCatID,true);
-        xmlhttp.send();
-    }
-
-    function showBrandProducts(brandcode,procatid) // show products from brand
-    {
-        var xmlhttp;
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                document.getElementById('resultTable').innerHTML=xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET","pos/includes/searchProcessForAll.php?id=brnd&brandCode="+brandcode+"&procatid="+procatid,true);
-        xmlhttp.send();
-    }
-</script>  
 
 <div class="main_text_box">
     <div style="padding-left: 112px;"><a href="personal_reporting.php"><b>ফিরে যান</b></a></div>
@@ -188,11 +22,9 @@ function get_catagory() {
                             <table>
                                 <tr>
                                     <td style="padding-left: 0px; text-align: left;" >শুরুর তারিখঃ</td>
-                                    <td style="text-align: left"><input class="box" type="date" name="loanamount" id="loanamount" /></td>	 
-
+                                    <td style="text-align: left"><input class="box" type="date" name="startDate" /></td>	 
                                     <td style="padding-left: 0px; text-align: left;"  >শেষের তারিখঃ</td>
-                                    <td style=" text-align: left"><input class="box" type="date" name="loanamount" id="loanamount" /></td>
-
+                                    <td style=" text-align: left"><input class="box" type="date" name="lastDate" /></td>
                                     <td style="padding-left: 50px; " ><input class="btn" style =" font-size: 12px; " type="submit" name="submit" value="সার্চ" /></td>
                                 </tr>
                             </table>
@@ -207,39 +39,65 @@ function get_catagory() {
                                 <table style="width: 98%;margin: 0 auto;" cellspacing="0" cellpadding="0">
                                     <thead>
                                         <tr id="table_row_odd">
-                                            <td width="11%" style="border: solid black 1px;"><div align="center"><strong>তারিখ</strong></div></td>
-                                            <td width="20%"  style="border: solid black 1px;"><div align="center"><strong>সময়</strong></div></td>
-                                            <td width="30%"  style="border: solid black 1px;"><div align="center"><strong>রশিদ নং</strong></div></td>
-                                            <td width="11%"  style="border: solid black 1px;"><div align="center"><strong>এমাউন্ট / মূল্য(টাকা)</strong></div></td>
-                                            <td width="12%" style="border: solid black 1px;"><div align="center"><strong>রেফারারের নাম</strong></div></td>
-                                            <td width="12%" style="border: solid black 1px;"><div align="center"><strong>পাকেজ</strong></div></td>
+                                            <td width="16%" style="border: solid black 1px;"><div align="center"><strong>তারিখ</strong></div></td>
+                                            <td width="12%"  style="border: solid black 1px;"><div align="center"><strong>সময়</strong></div></td>
+                                            <td width="25%"  style="border: solid black 1px;"><div align="center"><strong>রশিদ নং</strong></div></td>
+                                            <td width="11%"  style="border: solid black 1px;"><div align="center"><strong>মূল্য(টাকা)</strong></div></td>
+                                            <td width="20%" style="border: solid black 1px;"><div align="center"><strong>রেফারারের নাম</strong></div></td>
+                                            <td width="12%" style="border: solid black 1px;"><div align="center"><strong>প্যাকেজ</strong></div></td>
                                             <td width="12%" style="border: solid black 1px;"><div align="center"><strong>পিভি</strong></div></td>
                                         </tr>
                                     </thead>
                                     <tbody style="background-color: #FCFEFE">
                                         <?php
-                                        $slNo = 1;
-                                        $userID = $_SESSION['userIDUser'];
-                                        $result = mysql_query("SELECT *
-                                                                    FROM `sales_summary` 
-                                                                    WHERE sal_buyerid = $userID
-                                                                    ORDER BY sal_salesdate");
-                                        while ($row = mysql_fetch_assoc($result)) {
-                                            $db_sal_salesdate = $row["sal_salesdate"];
-                                            $db_sal_salestime = $row["sal_salestime"];
-                                            $db_sal_totalamount = $row["sal_totalamount"];
-                                            $db_sal_invoiceno = $row["sal_invoiceno"];
-                                            $db_sal_totalpv = $row['sal_totalpv'];
-                                            echo '<tr>';
-                                            echo '<td  style="border: solid black 1px;"><div align="center">' . $db_sal_salesdate . '</div></td>';
-                                            echo '<td  style="border: solid black 1px;"><div align="left">' . $db_sal_salestime . '</div></td>';
-                                            echo '<td  style="border: solid black 1px;"><div align="left">&nbsp;&nbsp;&nbsp;' . $db_sal_invoiceno . '</div></td>';
-                                            echo '<td  style="border: solid black 1px;"><div align="center">' . $db_sal_totalamount . '</div></td>';
-                                            echo '<td  style="border: solid black 1px;"><div align="center">' . $db_unit . '</div></td>';
-                                            echo '<td  style="border: solid black 1px;"><div align="center">' . $db_sal_totalpv . '</div></td>';
-                                            echo '<td  style="border: solid black 1px;"><div align="center">' . $db_sal_totalpv . '</div></td>';
-                                            echo '</tr>';
-                                            $slNo++;
+                                        if(isset($_POST['submit']))
+                                        {
+                                            $p_startdate = $_POST['startDate'];
+                                            $p_lastDate = $_POST['lastDate'];
+                                            $slNo = 1;
+                                            $userID = $_SESSION['userIDUser'];
+                                            $result = mysql_query("SELECT * FROM sales_summary WHERE sal_buyerid = $userID 
+                                                                                AND sal_salesdate >= '$p_startdate' AND sal_salesdate <= '$p_lastDate' ORDER BY sal_salesdate");
+                                            while ($row = mysql_fetch_assoc($result)) {
+                                                $db_sal_salesdate = $row["sal_salesdate"];
+                                                $db_sal_salestime = $row["sal_salestime"];
+                                                $db_sal_totalamount = $row["sal_totalamount"];
+                                                $db_sal_invoiceno = $row["sal_invoiceno"];
+                                                $db_sal_totalpv = $row['sal_totalpv'];
+                                                echo '<tr>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center">' . english2bangla(date("d/m/Y",  strtotime($db_sal_salesdate))) . '</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="left">' . english2bangla(date('g:i a' , strtotime($db_sal_salestime))) . '</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="left">&nbsp;&nbsp;&nbsp;' . $db_sal_invoiceno . '</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center">' . $db_sal_totalamount . '</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center"></div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center">??</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center">' . $db_sal_totalpv . '</div></td>';
+                                                echo '</tr>';
+                                                $slNo++;
+                                            }
+                                        }
+                                        else
+                                        {
+                                            $slNo = 1;
+                                            $userID = $_SESSION['userIDUser'];
+                                            $result = mysql_query("SELECT * FROM sales_summary WHERE sal_buyerid = $userID ORDER BY sal_salesdate");
+                                            while ($row = mysql_fetch_assoc($result)) {
+                                                $db_sal_salesdate = $row["sal_salesdate"];
+                                                $db_sal_salestime = $row["sal_salestime"];
+                                                $db_sal_totalamount = $row["sal_totalamount"];
+                                                $db_sal_invoiceno = $row["sal_invoiceno"];
+                                                $db_sal_totalpv = $row['sal_totalpv'];
+                                                echo '<tr>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center">' . english2bangla(date("d/m/Y",  strtotime($db_sal_salesdate))) . '</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="left">' . english2bangla(date('g:i a' , strtotime($db_sal_salestime))) . '</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="left">&nbsp;&nbsp;&nbsp;' . $db_sal_invoiceno . '</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center">' . $db_sal_totalamount . '</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center"></div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center">??</div></td>';
+                                                echo '<td  style="border: solid black 1px;"><div align="center">' . $db_sal_totalpv . '</div></td>';
+                                                echo '</tr>';
+                                                $slNo++;
+                                            }
                                         }
                                         ?>
                                     </tbody>
