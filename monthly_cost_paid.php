2b87baa00ac9be5d725c2c2fa078c84929c85876
jesy <jesy@jesy-PC>
2014-03-16 16:46:26 +0600

    commit by jesy

--- monthly_cost_paid.php
+++ monthly_cost_paid.php
@@ -4,12 +4,15 @@ error_reporting(0);
 include_once 'includes/header.php';
 include_once 'includes/MiscFunctions.php';
  $loginUSERid = $_SESSION['userIDUser'] ;
+ $logedinOfficeId = $_SESSION['loggedInOfficeID'];
+ 
  $g_ons_exp_id = $_GET['id'];
  $g_nfcid = $_GET['nfcid'];
 
 $sql_update_notification = $conn->prepare("UPDATE notification SET nfc_status=? WHERE idnotification=? ");
 $sql_fixed_expenditure = $conn->prepare("UPDATE ons_fixed_expenditure SET status='given' WHERE idfixexp=? ");
 $sel_fixed_exp = $conn->prepare("SELECT * FROM ons_fixed_expenditure WHERE idfixexp= ? AND 	status = 'approved' ");
+$ins_daily_inout = $conn->prepare("INSERT INTO acc_ofc_daily_inout (daily_date, daily_onsid, out_amount) VALUES (NOW(),?,?)");
 // ************************* select query ****************************************
 $sel_fixed_exp->execute(array($g_ons_exp_id));
 $row = $sel_fixed_exp->fetchAll();
@@ -22,12 +25,21 @@ foreach ($row as $value) {
 
 if(isset($_POST['submit']))
 {
-     $conn->beginTransaction(); 
+    $sel_onsID = $conn->prepare("SELECT idons_relation FROM ons_relation WHERE add_ons_id = ? AND catagory='office'");
+    $sel_onsID->execute(array($logedinOfficeId));
+    $offrow = $sel_onsID->fetchAll();
+    foreach ($offrow as $value) {
+       $office_ons_id = $value['idons_relation'];
+    }
+    
+    $p_total = $_POST['total'];
+    $conn->beginTransaction(); 
     $sqlrslt1= $sql_fixed_expenditure->execute(array($g_ons_exp_id ));
     $status = 'complete';
     $sqlrslt3 = $sql_update_notification->execute(array($status,$g_nfcid));
+    $insert = $ins_daily_inout->execute(array($office_ons_id,$p_total));
     
-     if($sqlrslt1 && $sqlrslt3)
+     if($sqlrslt1 && $sqlrslt3 && $insert)
         {
             $conn->commit();
             echo "<script>alert('খরচ করা হল');
@@ -49,7 +61,7 @@ if(isset($_POST['submit']))
                 <table  class="formstyle" style="width: 90%; margin: 1px 1px 1px 1px;">          
                     <tr><th colspan="2" style="text-align: center;font-size: 22px;">মাসিক খরচের টাকা গ্রহন</th></tr>
                     <tr>
-                        <td colspan="2" style="text-align: center;font-size: 16px;">
+                        <td colspan="2" style="text-align: center;font-size: 16px;"><input type="hidden" name="total" value="<?php echo $db_monthlytotal;?>" />
                             <?php echo $monthName." , ".$db_year?>-এর মাসিক খরচ বাবদ <?php echo $db_monthlytotal?> টাকা গ্রহন করা হল
                         </td>          
                     </tr>
