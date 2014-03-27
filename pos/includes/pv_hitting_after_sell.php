b7e7c69e3b648de275823abf9d9afe08bcef3c3c
jesy <jesy@jesy-PC>
2014-03-27 16:35:00 +0600

    commit by jesy

--- pos/includes/pv_hitting_after_sell.php
+++ pos/includes/pv_hitting_after_sell.php
@@ -94,20 +94,49 @@ function pv_hitting($custID, $cust_type, $sumID,$selling_type,$total_profit)
             $one_hit = ($total_profit * $Rone) / 100;
             $sql1 = mysql_query("UPDATE acc_user_balance SET pv_balance = pv_balance + $one_hit, total_balanace = total_balanace + $one_hit WHERE cfs_user_iduser = $one ");
             $sql5 = mysql_query("UPDATE main_fund SET fund_amount = fund_amount + $one_hit, last_update = NOW() WHERE fund_code = 'RHC'");
+            $sel_child_row = mysql_query("SELECT * FROM cust_pv_child_date WHERE cust_own_id = $one AND date = CURDATE() ");
+            if(mysql_num_rows($sel_child_row)> 0)
+            {
+               $sql6 = mysql_query("UPDATE cust_pv_child_date SET cust_c1 = cust_c1+$one_hit WHERE cust_own_id = $one AND date = CURDATE() ");
+            }
+            else
+                {
+                    $sql6 = mysql_query("INSERT INTO cust_pv_child_date (cust_own_id,cust_c1,date) VALUES ($one,$one_hit,NOW())");
+                }
         }
-        else { $borkot = $borkot + (($total_profit * $Rone) / 100); $one_hit = 0; $sql1=1; $sql5=1; }
+        else { $borkot = $borkot + (($total_profit * $Rone) / 100); $one_hit = 0; $sql1=1; $sql5=1; $sql6 =1;}
+        
         if($two != 0)
         {
             $two_hit = ($total_profit * $Rtwo) / 100;
             $sql1=mysql_query("UPDATE acc_user_balance SET pv_balance = pv_balance + $two_hit, total_balanace = total_balanace + $two_hit WHERE cfs_user_iduser = $two ");
             $sql5 = mysql_query("UPDATE main_fund SET fund_amount = fund_amount + $two_hit, last_update = NOW() WHERE fund_code = 'RHC'");
+            $sel_child_row = mysql_query("SELECT * FROM cust_pv_child_date WHERE cust_own_id = $two AND date = CURDATE()");
+            if(mysql_num_rows($sel_child_row)> 0)
+            {
+               $sql6 = mysql_query("UPDATE cust_pv_child_date SET cust_c2 = cust_c2+$two_hit WHERE cust_own_id = $two AND date = CURDATE()");
+            }
+            else
+                {
+                    $sql6 = mysql_query("INSERT INTO cust_pv_child_date (cust_own_id,cust_c2,date) VALUES ($two,$two_hit,NOW())");
+                }
         }
         else { $borkot = $borkot + (($total_profit * $Rtwo) / 100); $two_hit = 0;  }
+        
         if($three != 0)
         {
             $three_hit = ($total_profit * $Rthree) / 100;
             $sql1 = mysql_query("UPDATE acc_user_balance SET pv_balance = pv_balance + $three_hit, total_balanace = total_balanace + $three_hit WHERE cfs_user_iduser = $three ");
             $sql5 = mysql_query("UPDATE main_fund SET fund_amount = fund_amount + $three_hit, last_update = NOW() WHERE fund_code = 'RHC'");
+            $sel_child_row = mysql_query("SELECT * FROM cust_pv_child_date WHERE cust_own_id = $three AND date = CURDATE()");
+            if(mysql_num_rows($sel_child_row)> 0)
+            {
+               $sql6 = mysql_query("UPDATE cust_pv_child_date SET cust_c3 = cust_c3+$three_hit WHERE cust_own_id = $three AND date = CURDATE()");
+            }
+            else
+                {
+                    $sql6 = mysql_query("INSERT INTO cust_pv_child_date (cust_own_id,cust_c3,date) VALUES ($three,$three_hit,NOW())");
+                }
         }
         else { $borkot = $borkot + (($total_profit * $Rthree) / 100); $three_hit = 0;  }
         if($four != 0)
@@ -115,6 +144,15 @@ function pv_hitting($custID, $cust_type, $sumID,$selling_type,$total_profit)
             $four_hit = ($total_profit * $Rfour) / 100;
             $sql1 = mysql_query("UPDATE acc_user_balance SET pv_balance = pv_balance + $four_hit, total_balanace = total_balanace + $four_hit WHERE cfs_user_iduser = $four ");
             $sql5 = mysql_query("UPDATE main_fund SET fund_amount = fund_amount + $four_hit, last_update = NOW() WHERE fund_code = 'RHC'");
+            $sel_child_row = mysql_query("SELECT * FROM cust_pv_child_date WHERE cust_own_id = $four AND date = CURDATE()");
+            if(mysql_num_rows($sel_child_row)> 0)
+            {
+               $sql6 = mysql_query("UPDATE cust_pv_child_date SET cust_c4 = cust_c4+$four_hit WHERE cust_own_id = $four AND date = CURDATE()");
+            }
+            else
+                {
+                    $sql6 = mysql_query("INSERT INTO cust_pv_child_date (cust_own_id,cust_c4,date) VALUES ($four,$four_hit,NOW())");
+                }
         }
         else { $borkot = $borkot + (($total_profit * $Rfour) / 100); $four_hit = 0;  }
         if($five != 0)
@@ -122,8 +160,18 @@ function pv_hitting($custID, $cust_type, $sumID,$selling_type,$total_profit)
             $five_hit = ($total_profit * $Rfive) / 100;
             $sql1 = mysql_query("UPDATE acc_user_balance SET pv_balance = pv_balance + $five_hit, total_balanace = total_balanace + $five_hit WHERE cfs_user_iduser = $five ");
             $sql5 = mysql_query("UPDATE main_fund SET fund_amount = fund_amount + $five_hit, last_update = NOW() WHERE fund_code = 'RHC'");
+            $sel_child_row = mysql_query("SELECT * FROM cust_pv_child_date WHERE cust_own_id = $five AND date = CURDATE()");
+            if(mysql_num_rows($sel_child_row)> 0)
+            {
+               $sql6 = mysql_query("UPDATE cust_pv_child_date SET cust_c5 = cust_c5+$five_hit WHERE cust_own_id = $five AND date = CURDATE()");
+            }
+            else
+                {
+                    $sql6 = mysql_query("INSERT INTO cust_pv_child_date (cust_own_id,cust_c5,date) VALUES ($five,$five_hit,NOW())");
+                }
         }
         else { $borkot = $borkot + (($total_profit * $Rfive) / 100); $five_hit = 0;  }
+        
             // calculate total soft cost ****************************     
            foreach ($arr_softcost as $key => $value) {
                $sql2 = mysql_query("UPDATE main_fund SET fund_amount = fund_amount + $value, last_update = NOW() WHERE fund_code = '$key' ");
@@ -132,13 +180,23 @@ function pv_hitting($custID, $cust_type, $sumID,$selling_type,$total_profit)
            $se_hit = ($total_profit * $se) / 100;
            $ri_hit = ($total_profit * $ri) / 100;
            $own_hit = ($total_profit * $direct_sales) / 100;
+           
+           $sel_own_row = mysql_query("SELECT * FROM cust_pv_child_date WHERE cust_own_id = $custID AND date = CURDATE()");
+            if(mysql_num_rows($sel_own_row)> 0)
+            {
+               $sql7 = mysql_query("UPDATE cust_pv_child_date SET cust_own_pv = cust_own_pv+$own_hit WHERE cust_own_id = $custID AND date = CURDATE()");
+            }
+            else
+                {
+                    $sql7 = mysql_query("INSERT INTO cust_pv_child_date (cust_own_id,cust_own_pv,date) VALUES ($custID,$own_hit,NOW())");
+                }
 
            $sql4 = mysql_query("UPDATE acc_user_balance SET pv_balance = pv_balance + $own_hit, total_balanace = total_balanace + $own_hit WHERE cfs_user_iduser = $custID ");
 
            $sql3 = mysql_query("INSERT INTO sales_customer_hitting (selling_earn,soft_costing,own,Rone,Rtwo,Rthree,Rfour,Rfive,ripd_income,borkot,sales_summery_idsalessummery) 
                                              VALUES($se_hit,$total_softcost,$own_hit,$one_hit,$two_hit,$three_hit,$four_hit,$five_hit,$ri_hit,$borkot,$sumID)") or exit(mysql_error());
 
-           if($sql1 && $sql2 && $sql3 && $sql4 && $sql5)
+           if($sql1 && $sql2 && $sql3 && $sql4 && $sql5 && $sql6 && $sql7)
            {
                 mysql_query("COMMIT");
                 $flag = 1;
