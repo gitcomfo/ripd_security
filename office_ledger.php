efadd4dc569cfde7012587c4fd3e424121d43cba
jesy <jesy@jesy-PC>
2014-05-20 17:25:14 +0600

    commit by jesy

---
+++ office_ledger.php
@@ -0,0 +1,104 @@
+<?php
+//error_reporting(0);
+include_once 'includes/session.inc';
+include_once 'includes/header.php';
+include_once 'includes/selectQueryPDO.php';
+$ons_type = $_SESSION['loggedInOfficeType'];
+$ons_id = $_SESSION['loggedInOfficeID'];
+
+$sql_select_id_ons_relation->execute(array($ons_type,$ons_id));
+$row = $sql_select_id_ons_relation->fetchAll();
+foreach ($row as $onsrow) {
+    $db_onsID = $onsrow['idons_relation'];
+}
+
+$sel_daily_inout = $conn->prepare();
+$current_month = date('m');
+$current_year = date('Y');
+$monthName = date("F", mktime(0, 0, 0, $current_month, 10));
+
+if(isset($_POST['submit']))
+{
+    $current_month = $_POST['month'];
+    $current_year = $_POST['year'];
+    $monthName = date("F", mktime(0, 0, 0, $current_month, 10));
+}
+
+$sel_ofc_ledger = mysql_query("SELECT * FROM acc_ofc_physc_ledger WHERE month_no= $current_month 
+                                                    AND year_no = $current_year  AND ripd_office_id= $db_onsID");
+
+    while($row = mysql_fetch_assoc($sel_ofc_ledger)) {
+        $db_ripd_amount = $row['ripd_amount'];
+        $db_out_amount = $row['out_amount'];
+        $db_in_amount = $row['in_amount'];
+        $db_total_amount = $row['total_amount'];
+    }
+?>
+<style type="text/css"> @import "css/bush.css";</style>
+
+<div class="columnSld" style=" padding-left: 50px;">
+    <div class="main_text_box">
+        <div style="padding-left: 9px;"><a href="accounting_sys_management.php"><b>ফিরে যান</b></a></div>
+        <div>           
+                <table  class="formstyle" style="width: 90%; margin: 1px 1px 1px 1px;">          
+                  <tr><th colspan="2" style="text-align: center;font-size: 22px;">অফিসের মাসিক লেজার</th></tr>                    
+                    <tr>
+                      <td>
+                          <form method="POST" action="">
+                        <b>মাসঃ </b>
+                            <select class='box2' name='month' style="width: 100px;" id="month">
+                                <?php
+                                $inc = 1;
+                                $month_array = array(0=>'সিলেক্ট করুন','01'=>'জানুয়ারি','02'=>'ফেব্রুয়ারি', '03' =>'মার্চ', '04'=>'এপ্রিল', '05'=>'মে', '06'=> 'জুন', '07'=> 'জুলাই', '08'=>'আগষ্ট', '09'=> 'সেপ্টেম্বর','10'=> 'অক্টোবর','11'=> 'নভেম্বর','12'=> 'ডিসেম্বর');
+                                while (list ($inc, $val) = each($month_array))
+                                    echo "<option value=$inc>$val</option>";
+                                ?>
+                            </select>&nbsp;&nbsp;
+                            <b>বছরঃ </b>
+                            <select class='box2' name='year'>
+                                <?php
+                                    $thisYear = date('Y');
+                                    $startYear = '2000';
+                                    foreach (range($thisYear, $startYear) as $year) {
+                                    echo '<option value='.$year.'>'. $year .'</option>'; }
+                                ?>
+                            </select> &nbsp;&nbsp;&nbsp;
+                            <input class="btn" style =" font-size: 12px; " type="submit" name="submit" value="সার্চ" />
+                          </from>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td>মাস : </td>
+                                    <td><?php echo $monthName?></td>
+                                </tr>
+                                <tr>
+                                    <td>বছর : </td>
+                                    <td><?php echo $current_year?></td>
+                                </tr>
+                                <tr>
+                                    <td>রিপড এমাউন্ট : </td>
+                                    <td><?php echo $db_ripd_amount?></td>
+                                </tr>
+                                <tr>
+                                    <td>ইন এমাউন্ট : </td>
+                                    <td><?php echo $db_in_amount?></td>
+                                </tr>
+                                <tr>
+                                    <td>আউট এমাউন্ট : </td>
+                                    <td><?php echo $db_out_amount?></td>
+                                </tr>
+                                <tr>
+                                    <td>মোট : </td>
+                                    <td><?php echo $db_total_amount?></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+        </div>
+    </div>
+</div>
+<?php include 'includes/footer.php'; ?>
