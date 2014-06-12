c6b391ca3b9c8da68da4b30e4faef79f2be54128
jesy <jesy@jesy-PC>
2014-06-12 16:36:22 +0600

    commit by jesy

--- includes/headContent_css_js_others.php
+++ includes/headContent_css_js_others.php
@@ -1,6 +1,7 @@
             <link rel="icon" type="image/png" href="images/favicon.png" />
             <link rel="stylesheet" type="text/css" href="css/style.css" />  
             <script type="text/javascript" src="javascripts/domtab.js"></script>
+            <script type="text/javascript" src="javascripts/jquery-1.4.3.min.js"></script>
 <!--            <script type="text/javascript" src="javascripts/external/mootools.js"></script>
             <script type="text/javascript" src="javascripts/demo-js/table.js"></script>
             <script type="text/javascript" src="javascripts/dg-filter.js"></script>
@@ -8,3 +9,14 @@
             <script type="text/javascript" src="javascripts/jquery.js"></script>
             <script src="javascripts/tooltip/jquery.js" type="text/javascript"></script>
             <script src="javascripts/tooltip/main.js" type="text/javascript"></script>-->
+            
+            <script type="text/javascript" charset="utf-8">
+                  $(document).ready(function(){
+                    $(".nav a").each(function(){
+                        var urlstr = '/ripd_security/'+$(this).attr('href');
+                         if(urlstr ===  window.location.pathname){
+                            $(this).addClass('current');
+                        }
+                    });
+                   });
+            </script>
