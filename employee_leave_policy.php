319602487b0664f86ce466ae8fe81c8c65540438
jesy <jesy@jesy-PC>
2014-06-16 12:36:20 +0600

    commit by jesy

--- employee_leave_policy.php
+++ employee_leave_policy.php
@@ -2,7 +2,6 @@
 error_reporting(0);
 include_once 'includes/session.inc';
 include_once 'includes/header.php';
-include_once 'includes/MiscFunctions.php';
 
 $loginUSERname = $_SESSION['UserID'] ;
 $queryemp = mysql_query("SELECT idUser FROM cfs_user WHERE user_name = '$loginUSERname';");
@@ -57,7 +56,6 @@ if(isset($_POST['submit']))
    else {$msg = "ছুটি তৈরি হয়নি";}
 }
 ?>
-<title>কর্মচারীর ছুটি</title>
 <style type="text/css">@import "css/bush.css";</style>
 <link rel="stylesheet" type="text/css" media="all" href="javascripts/jsDatePick_ltr.min.css" />
 <script type="text/javascript" src="javascripts/jsDatePick.min.1.3.js"></script>
@@ -134,7 +132,7 @@ function getEmployee(keystr) //search employee by account number***************
                }
                 else
                     {document.getElementById('empfound').style.visibility = "visible";
-                document.getElementById('empfound').setAttribute('style','position:absolute;top:41.5%;left:61.5%;width:225px;z-index:10;padding:5px;border: 1px inset black; overflow:auto; height:105px; background-color:#F5F5FF;');
+                document.getElementById('empfound').setAttribute('style','position:absolute;top:43%;left:62.5%;width:225px;z-index:10;padding:5px;border: 1px inset black; overflow:auto; height:105px; background-color:#F5F5FF;');
                     }
                 document.getElementById('empfound').innerHTML=xmlhttp.responseText;
         }
@@ -183,31 +181,31 @@ function getEmployee(keystr) //search employee by account number***************
                         <td width="59%">
                             <table>
                                  <tr>
-                        <td width="30%" >ছুটির টাইপ</td>
-                        <td width="70%">:  <select class="textfield" onchange="getLeaveInGrade(this.value,'<?php echo $empCfsid;?>')" name="leave_type" style =" font-size: 11px">
-                                <?php echo getLeaveType(); ?></select></td>	 
-                    </tr>  
-                    <tr>
-                        <td >ছুটি শুরুর তারিখ</td>
-                        <td>:  <input class="textfield" type="text" id="date" placeholder="Date" name="startdate" value=""/></td>	  
-                    </tr>  
-                    <tr>
-                        <td >ছুটির দিনের সংখ্যা</td>
-                        <td>:  <input class="textfield" type="text" id="leavedays" name="leavedays" onkeypress="return checkIt(event)" onkeyup="checkmaxdays(this.value)"/> দিন
-                            (সর্বোচ্চ <input type="text" readonly="" id="maxdays" style="width: 15px;color: red;" /> দিন)</td>	  
-                    </tr>  
-                    <tr>
-                        <td >বর্ণনা</td>
-                        <td><textarea class="textfield" type="text" id="leavedes" name="leavedes"></textarea></td>
-                    </tr> 
-                    <tr>
-                        <td >স্ক্যান ডকুমেন্টস</td>
-                        <td>:   <input class="box" type="file" id="leavescan" name="leavescan" style="font-size:10px;"/></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"></br><input class="btn" style =" font-size: 12px;margin-left: 50px;visibility: hidden;" type="submit" id="submit" name="submit" value="সেভ করুন" />
-                                <input class="btn" style =" font-size: 12px" type="reset" name="reset" value="রিসেট করুন" /></td>
-                    </tr>
+                                    <td width="30%" >ছুটির টাইপ</td>
+                                    <td width="70%">:  <select class="textfield" onchange="getLeaveInGrade(this.value,'<?php echo $empCfsid;?>')" name="leave_type" style =" font-size: 11px">
+                                            <?php echo getLeaveType(); ?></select></td>	 
+                                </tr>  
+                                <tr>
+                                    <td >ছুটি শুরুর তারিখ</td>
+                                    <td>:  <input class="textfield" type="text" id="date" placeholder="Date" name="startdate" value=""/></td>	  
+                                </tr>  
+                                <tr>
+                                    <td >ছুটির দিনের সংখ্যা</td>
+                                    <td>:  <input class="textfield" type="text" id="leavedays" name="leavedays" onkeypress="return checkIt(event)" onkeyup="checkmaxdays(this.value)"/> দিন
+                                        (সর্বোচ্চ <input type="text" readonly="" id="maxdays" style="width: 15px;color: red;" /> দিন)</td>	  
+                                </tr>  
+                                <tr>
+                                    <td >বর্ণনা</td>
+                                    <td><textarea class="textfield" type="text" id="leavedes" name="leavedes"></textarea></td>
+                                </tr> 
+                                <tr>
+                                    <td >স্ক্যান ডকুমেন্টস</td>
+                                    <td>:   <input class="box" type="file" id="leavescan" name="leavescan" style="font-size:10px;"/></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2"></br><input class="btn" style =" font-size: 12px;margin-left: 50px;visibility: hidden;" type="submit" id="submit" name="submit" value="সেভ করুন" />
+                                            <input class="btn" style =" font-size: 12px" type="reset" name="reset" value="রিসেট করুন" /></td>
+                                </tr>
                             </table>
                         </td>
                            <td width="41%">
@@ -215,32 +213,27 @@ function getEmployee(keystr) //search employee by account number***************
                                  <tr>
                                      <td colspan="8" style="text-align: right">খুঁজুন:  <input type="text" class="box" style="width: 230px;" id="empsearch" name="empsearch" onkeyup="getEmployee(this.value)"/>
                                     <div id="empfound"></div></td>
-                            </tr>
-                            <tr>
-                                <td width="40%" rowspan='4' style="padding-left: 0px;"> <img src="<?php echo $db_empphoto;?>" width="128px" height="128px" alt=""></td> 
-                            </tr>
-                            <tr>
-                                <td width="57%"><input type="hidden" readonly="" value="<?php echo $db_empname;?>" /><?php echo $db_empname;?></td>
-                            </tr>     
-                            <tr>
-                                <td><input type="hidden" readonly="" value="<?php echo $db_empposition;?>" /><?php echo $db_empposition;?>
-                                    <input type="hidden" readonly="" id="emp_paygrade" name="emp_paygrade" value="<?php echo $db_paygrdid;?>" /></td>
-                            </tr>    
-                            <tr>
-                                <td height="74"><input type="hidden" readonly="" value="<?php echo $db_empmobile;?>" /><?php echo $db_empmobile;?>
-                                <input type="hidden" readonly="" name="cfsid"value="<?php echo $empCfsid;?>" /></td>
-                            </tr>                 
+                                </tr>
+                                <tr>
+                                    <td width="40%" rowspan='4' style="padding-left: 0px;"> <img src="<?php echo $db_empphoto;?>" width="128px" height="128px" alt=""></td> 
+                                </tr>
+                                <tr>
+                                    <td width="57%"><input type="hidden" readonly="" value="<?php echo $db_empname;?>" /><?php echo $db_empname;?></td>
+                                </tr>     
+                                <tr>
+                                    <td><input type="hidden" readonly="" value="<?php echo $db_empposition;?>" /><?php echo $db_empposition;?>
+                                        <input type="hidden" readonly="" id="emp_paygrade" name="emp_paygrade" value="<?php echo $db_paygrdid;?>" /></td>
+                                </tr>    
+                                <tr>
+                                    <td height="74"><input type="hidden" readonly="" value="<?php echo $db_empmobile;?>" /><?php echo $db_empmobile;?>
+                                    <input type="hidden" readonly="" name="cfsid"value="<?php echo $empCfsid;?>" /></td>
+                                </tr>                 
                             </table>
                         </td>
-                    </tr>
-                    <tr>
-                     
                     </tr>
             </table>
         </form>
     </div>
     </div>
 </div>
-<?php 
-include_once 'includes/footer.php'; 
-?>
+<?php include_once 'includes/footer.php'; ?>
